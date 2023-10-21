879dc2a0c006354e68fcc4057545754bbd2bb444
rahul <rahul@justfreshjobs.com>
2023-10-21 15:01:20 +0530

    animation added on header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import Dashboard from './Dashboard';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App flex w-screen h-screen overflow-hidden">
         <Dashboard />
+
+        <div className="main-area flex items-start w-full">
+          <Header />
+        </div>
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,48 @@
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+import "./css/header.css";
+import { useRef } from 'react';
+
+export default function Header() {
+  const holder: any = useRef(null);
+
+  const activateInput = () => {
+    (holder.current as HTMLDivElement).classList.add("input");
+
+    function revertInput(e: any): void {
+      if (!(
+        e.target.classList.contains("search-div") || 
+        e.target.parentElement.classList.contains("search-div") || 
+        e.target.parentElement.parentElement.classList.contains("search-div")
+      ))
+      {
+        (holder.current as HTMLDivElement).classList.remove("input");
+        document.body.removeEventListener("click", revertInput);
+      }
+    }
+
+    document.body.addEventListener("click", revertInput);
+  }
+  return (
+    <header className='flex w-full items-center p-4'>
+      <div className="flex items-center w-fit">
+        <button className="hamburger">
+          <MenuOutlinedIcon />
+        </button>
+      </div>
+
+      <div ref={holder} className="holder flex items-center ml-auto">
+        <div className="search-div">
+          <SearchOutlinedIcon />
+          <input type="text" placeholder='Search...' onClick={activateInput} />
+        </div>
+
+        <div className="notification" onClick={(e: any) => e.currentTarget.classList.toggle("active")}>
+          <NotificationsNoneOutlinedIcon />
+          <span className="red" />
+        </div>
+      </div>
+    </header>
+  )
+}
